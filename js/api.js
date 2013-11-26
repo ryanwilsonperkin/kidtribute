be568d38a246c0e0530f0c7d18d7921e91d6a4fa
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2013-11-26 13:24:34 -0500

    add javascript function stubs for api calls

---
+++ js/api.js
@@ -0,0 +1,75 @@
+function Login(username, password) {
+
+}
+
+function GetUser(userId) {
+
+}
+
+function CreateUser(user) {
+
+}
+
+function UpdateUser(user) {
+
+}
+
+function VetCommunityMember(project) {
+
+}
+
+function GetProject(projectId) {
+
+}
+
+function CreateProject(project) {
+
+}
+
+function UpdateProject(project) {
+
+}
+
+function AddCommMember(projectId, userId) {
+
+}
+
+function ApproveProject(projectId) {
+
+}
+
+function RejectProject(projectId, comment) {
+
+}
+
+function GetAllUnapprovedProjects(principalId) {
+
+}
+
+function GetAllProjectsForTeacher(teacherId) {
+
+}
+
+function GetAllProjectsForPrincipal(principalId) {
+
+}
+
+function GetAllProjectsForCommunityMember(communityMemberId) {
+
+}
+
+function GetAllProjectsWhere(query, school, teacher, title, subject) {
+
+}
+
+function GetSchool(schoolId) {
+
+}
+
+function GetAllSchools(boardId) {
+
+}
+
+function GetProjectMembers(projectId) {
+
+}
