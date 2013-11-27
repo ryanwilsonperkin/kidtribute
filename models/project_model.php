cd32d16dd84ab6750c859f52c9ca5254a72ed040
dmacdo02 <dmacdo02@uoguelph.ca>
2013-11-26 23:50:53 -0500

    Update project_model.php

--- models/project_model.php
+++ models/project_model.php
@@ -9,7 +9,6 @@ class project_model extends db
 	function load($id=-1)
 	{
 		$query = "SELECT * FROM `Projects` WHERE `project_id`=" . $id;
-		echo $query;
 		$result = mysql_query($query) or die(mysql_error());
         $result = _parse_result($result);
         $user_object = new projectDB();
