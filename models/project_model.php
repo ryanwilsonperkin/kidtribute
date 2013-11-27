0a2934d715a10791226695809cd4ad266365f0d7
dmacdo02 <dmacdo02@uoguelph.ca>
2013-11-27 01:09:48 -0500

    Update project_model.php

--- models/project_model.php
+++ models/project_model.php
@@ -114,7 +114,7 @@ class project_model extends db
 		if ($project->imageUrl == null)
 			$project->imageUrl = "null";
 		if ($project->category_id == null)
-			$project->category_id = "null";
+			$project->category_id = 1;
 		
 		$query = "INSERT INTO `Projects` (school_id, teacher_id, category_id, title, description, startDate, endDate, imageUrl, approved) VALUES ($project->school_id, $project->teacher_id, $project->category_id, '$project->title', '$project->description', CAST ('$project->startDate' AS DATE), CAST('$project->endDate' AS DATE), '$project->imageUrl', $project->approved)";
 		echo $query;
