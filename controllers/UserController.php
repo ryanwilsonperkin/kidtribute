b4a8833ba85c7c1551c18359859260bf71730d25
dmacdo02 <dmacdo02@uoguelph.ca>
2013-11-26 22:49:54 -0500

    Update UserController.php
    ADding the use of username for authentication

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,10 +2,10 @@
 include_once dirname(__FILE__) . '/../models/user_model.php';
 include_once dirname(__FILE__) . '/../DBobjects/UserDBObject.php';
 
-function controller_Login($email, $password){
+function controller_Login($username, $password){
     //get a user from the database via their email address
     $user_model = new user_model();
-    $user_object = $user_model->load_from_email($email);
+    $user_object = $user_model->load_from_username($username);
 
     //verify the passwords match
     if ($user_object->password == $password) {
@@ -13,7 +13,7 @@ function controller_Login($email, $password){
                                          $user_object->user_type_id, $user_object->email,
                                          $user_object->password, $user_object->name,
                                          $user_object->title, "", $user_object->isVetted,
-                                         $user_object->bio, "", $user_object->imageUrl, "");
+                                         $user_object->bio, "", $user_object->imageUrl, $user_object->username);
 
         $response_object = new ResponseObject("200", "OK", null, $user_response);
     }
@@ -33,7 +33,7 @@ function controller_GetUser($userId){
                                          $user_object->user_type_id, $user_object->email,
                                          $user_object->password, $user_object->name,
                                          $user_object->title, "", $user_object->isVetted,
-                                         $user_object->bio, "", $user_object->imageUrl, "");
+                                         $user_object->bio, "", $user_object->imageUrl, $user_object->username);
 
         $response_object = new ResponseObject("200", "OK", null, $user_response);
     }
@@ -50,7 +50,7 @@ function controller_CreateUser($user_request)
     $user_db = new userDB($user_request->id, $user_request->schoolId, $user_request->userType,
                                 $user_request->email, $user_request->password, $user_request->name,
                                 $user_request->title, $user_request->isVetted, $user_request->bio,
-                                $user_request->imageUrl);
+                                $user_request->imageUrl, $user_object->username);
 
 	$user_model = new user_model();
 	$user_object = $user_model->create_user($user_db);
@@ -65,7 +65,7 @@ function controller_UpdateUser($user_request){
     $user_db = new userDB($user_request->id, $user_request->schoolId, $user_request->userType,
                                 $user_request->email, $user_request->password, $user_request->name,
                                 $user_request->title, $user_request->isVetted, $user_request->bio,
-                                $user_request->imageUrl);
+                                $user_request->imageUrl, $user_request->username);
 
     $user_model = new user_model();
     $user_object = $user_model->update_user($user_db);
